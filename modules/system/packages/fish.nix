eb497547d44182345ba35588ef31bef2f4ab087e
JPierro0513 <jmpierro0513@gmail.com>
2025-03-24 14:22:44 -0400

    Added transient fish prompt and some more aliases

--- modules/system/packages/fish.nix
+++ modules/system/packages/fish.nix
@@ -7,17 +7,33 @@
     forgit
     hydro
     pisces
+    transient-fish
   ];
 
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
       set fish_greeting # Disable greeting
+
+      function transient_prompt_func
+        set --local color green
+	if test $transient_pipestatus[-1] -ne 0
+	  set color red
+	end
+	echo -en (set_color brblack)"["(data "+%I:%M")"]" "(set_color $color)"❱ "(set_color normal)
+      end
+
+      set $fish_prompt_pwd_dir_length 3
+      set $hydro_multiline true
+      set $hydro_color_prompt green
+      set $hydro_color_duration orange
     '';
     shellAliases = {
       "update" ="sudo nixos-rebuild switch";
       "bupdate"="sudo nixos-rebuild boot";
       "ls"="eza --icons -1 --group-directories-first --git";
+      "lt"="ls --tree";
+      "cat"="bat";
     };
   };
 }
