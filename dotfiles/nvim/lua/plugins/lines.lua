6fa4e34598c1289816278d5af18de1315fa314ba
JPierro0513 <jmpierro0513@gmail.com>
2025-03-24 21:28:31 -0400

    Changed neovim statuslines

--- dotfiles/nvim/lua/plugins/lines.lua
+++ dotfiles/nvim/lua/plugins/lines.lua
@@ -3,31 +3,45 @@ local load,map = require 'utils'.load,require'utils'.map
 load('mini.tabline', { lazy = true, opts = {} })
 map('<leader>bd', function() load 'mini.bufremove'.delete(0, false) end, 'Close buffer')
 
-load('mini.statusline', {
+-- load('mini.statusline', {
+--     lazy = true,
+--     opts = {
+--         content = {
+--             active = function()
+--                 local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+--                 local git           = MiniStatusline.section_git({ trunc_width = 40 })
+--                 local diff          = MiniStatusline.section_diff({ trunc_width = 75 })
+--                 local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+--                 local lsp           = MiniStatusline.section_lsp({ trunc_width = 75 })
+--                 local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
+--                 local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 900 })
+--                 local location      = MiniStatusline.section_location({ trunc_width = 900 })
+--                 local search        = MiniStatusline.section_searchcount({ trunc_width = 75 })
+--
+--                 return MiniStatusline.combine_groups({
+--                     { hl = mode_hl,                 strings = { mode } },
+--                     { hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics, lsp } },
+--                     '%<', -- Mark general truncate point
+--                     { hl = 'MiniStatuslineFilename', strings = { filename } },
+--                     '%=', -- End left alignment
+--                     { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+--                     { hl = mode_hl,                  strings = { search, location } },
+--                 })
+--             end
+--         }
+--     }
+-- })
+
+load('sschleemilch/slimline.nvim', {
     lazy = true,
     opts = {
-        content = {
-            active = function()
-                local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
-                local git           = MiniStatusline.section_git({ trunc_width = 40 })
-                local diff          = MiniStatusline.section_diff({ trunc_width = 75 })
-                local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
-                local lsp           = MiniStatusline.section_lsp({ trunc_width = 75 })
-                local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
-                local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 900 })
-                local location      = MiniStatusline.section_location({ trunc_width = 900 })
-                local search        = MiniStatusline.section_searchcount({ trunc_width = 75 })
-
-                return MiniStatusline.combine_groups({
-                    { hl = mode_hl,                 strings = { mode } },
-                    { hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics, lsp } },
-                    '%<', -- Mark general truncate point
-                    { hl = 'MiniStatuslineFilename', strings = { filename } },
-                    '%=', -- End left alignment
-                    { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
-                    { hl = mode_hl,                  strings = { search, location } },
-                })
-            end
+        bold = true,
+        verbose_mode = true,
+        style = 'fg',
+        components = {
+            left = { 'mode', 'git', 'path' }, -- TODO: custom path component
+            center = {}, -- TODO: navic
+            right = { 'diagnostics', 'filetype_lsp', 'progress' }, -- TODO: custom progress
         }
-    }
+    },
 })
