741c2f2baa98a9063889c63f1e60e09f7e11ca0b
JPierro0513 <jmpierro0513@gmail.com>
2025-03-24 22:44:05 -0400

    Added some color

--- dotfiles/nvim/lua/plugins/colorscheme.lua
+++ dotfiles/nvim/lua/plugins/colorscheme.lua
@@ -3,10 +3,25 @@ local load = require'utils'.load
 load('vague2k/vague.nvim', { lazy = false })
 -- vim.cmd.colorscheme 'vague'
 
-load('slugbyte/lackluster.nvim', {
-    lazy = false,
-    opts = {
-
+load('slugbyte/lackluster.nvim', { lazy = false })
+local lackluster = require 'lackluster'
+lackluster.setup({
+    tweak_background = {
+        menu = 'none',
+        popup = 'none',
+        -- telescope = 'default',
+        -- normal = 'default',
     },
+    tweak_syntax = {
+        keyword = lackluster.color.lack,
+        string = '#FFAA88',
+        comment = lackluster.color.gray5,
+        type = lackluster.color.green,
+        builtin = lackluster.color.orange,
+        keyword_return = lackluster.color.red,
+        keyword_exception = lackluster.color.lack,
+        string_escape = lackluster.color.blue,
+    },
+    disable_plugin = {},
 })
 vim.cmd.colorscheme 'lackluster'
