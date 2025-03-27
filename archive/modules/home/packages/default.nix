c304d87623019b428cc4eae4a6b9d2a2e56fbb10
JPierro0513 <jmpierro0513@gmail.com>
2025-03-27 14:08:57 -0400

    I think its time

--- archive/modules/home/packages/default.nix
+++
@@ -1,8 +0,0 @@
-{
-  imports = [
-    ./desktop.nix
-    ./terminal.nix
-    ./games.nix
-    ./spicetify.nix
-  ];
-}
--- archive/modules/system/services/display-manager.nix
+++
@@ -1,8 +0,0 @@
-# {pkgs, ...}:
-{
-  # environment.systemPackages = with pkgs; [sddm-chili-theme];
-
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
-  # services.displayManager.sddm.theme = "chili";
-}
