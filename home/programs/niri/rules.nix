06a91c7c04171bc475bf4a84cf797f44ce944dc7
JPierro0513 <jmpierro0513@gmail.com>
2025-05-14 15:19:59 -0400

    Formatted file

--- home/programs/niri/rules.nix
+++ home/programs/niri/rules.nix
@@ -1,14 +1,16 @@
 {
   programs.niri.settings.window-rules = [
-      {
-        draw-border-with-background = false;
-        clip-to-geometry = true;
-        geometry-corner-radius = let radius = 12.0; in {
-	  bottom-left = radius;
-	  bottom-right = radius;
-	  top-left = radius;
-	  top-right = radius;
-	};
-      }
-    ];
+    {
+      draw-border-with-background = false;
+      clip-to-geometry = true;
+      geometry-corner-radius = let
+        radius = 12.0;
+      in {
+        bottom-left = radius;
+        bottom-right = radius;
+        top-left = radius;
+        top-right = radius;
+      };
+    }
+  ];
 }
