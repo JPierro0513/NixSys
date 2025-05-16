4e84ae39950e3640a0f88d36fe9dc2333505e777
JPierro0513 <jmpierro0513@gmail.com>
2025-05-16 00:14:09 -0400

    adding nushell config

---
+++ home/programs/nushell.nix
@@ -0,0 +1,47 @@
+{pkgs, ...}: {
+  programs.nushell = {
+    enable = true;
+    shellAliases = {
+      ls = "ls | sort-by type";
+      la = "ls -a";
+      l = "ls";
+      lt = "ls **/*";
+      fg = "job unfreeze";
+    };
+    extraConfig = ''
+      let carapace_completer = {|spans|
+        carapace $spans.0 nushell ...$spans | from json
+      }
+      $env.config = {
+        show_banner: false,
+	completions: {
+          case_sensitive: false
+	  quick: true
+	  partial: true
+	  algorithm: "fuzzy"
+	  external: {
+            enable: true
+	    max_results: 100
+	    completer: $carapace_completer
+	  }
+	}
+      }
+
+      zoxide init nushell --cmd cd | save -f ~/.zoxide.nu
+      alias fuck = thefuck $"(history | last 1 | get command | get 0)"
+    '';
+  };
+  carapace.enable = true;
+  carapace.enableNushellIntegration = true;
+
+  starship = { 
+    enable = true;
+    settings = {
+      add_newline = true;
+      character = { 
+        success_symbol = "[➜](bold green)";
+        error_symbol = "[➜](bold red)";
+      };
+    };
+  };
+}
